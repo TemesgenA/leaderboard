8154b5d0c4c915967017b68185028ea54bbdaa67
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-27 13:02:44 +0300

    import save score module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// import _ from 'lodash';
 import './style.css';
+
+import saveScores from './modules/save-scores.js';
+// import loadScores from './modules/load-scores.js';
+
+const saveScoresForm = document.getElementById('add-score-form');
+saveScoresForm.addEventListener('submit', saveScores);
