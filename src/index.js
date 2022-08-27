5e80f2cb30abeb89e9d35ac90433eafce18d4837
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-27 13:05:01 +0300

    clean the code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './style.css';
 
 import saveScores from './modules/save-scores.js';
-// import loadScores from './modules/load-scores.js';
+import loadScores from './modules/load-scores.js';
 
+// importing save-score module
 const saveScoresForm = document.getElementById('add-score-form');
 saveScoresForm.addEventListener('submit', saveScores);
+
+// importing load score module
+const refreshBtn = document.getElementById('refresh');
+refreshBtn.addEventListener('click', loadScores);
